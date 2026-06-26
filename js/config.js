97da98fe0063cf0ffe1a9b02aa1421bf3eca5ce9
stay86625-ui <stay86625@gmail.com>
2026-06-27 06:13:20 +0800

    deploy: js/config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+(function (G) {
+  'use strict';
+  G.W = 1920; G.H = 1080;
+  G.SCALE = 44; G.ISO_Z = 0.60; G.ISO_H = 0.85;
+  G.DAY_DUR = 360;
+  G.CHAR_CW = 20; G.CHAR_CH = 30; G.CHAR_SC = 3;
+  G.CHUNK_SIZE = 32;
+  G.LOAD_RADIUS = 2;
+  G.CLS_NAMES = ['法師', '劍士', '弓箭手', '伊格', '刺客'];
+})(window.Game = window.Game || {});
